15167487972b7e69f70e3913cd4cdb5bce3a5e50
Tony Proum <t.proum@gmail.com>
2016-10-03 12:16:30 +0200

    Enhance configuration by adding more purifycss and using gzip compression.
    This should help to reduce loads times.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component,AfterViewInit,ElementRef } from '@angular/core';
 let $ = require('../../node_modules/jquery/dist/jquery.js');
 let foundation = require('../../node_modules/foundation-sites/dist/foundation.js');
 $().foundation = foundation;
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
